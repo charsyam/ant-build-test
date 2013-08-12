968f4365b6bd891641de43995b71bae44a40d48c
charsyam <charsyam@gmail.com>
2013-08-13 08:37:14 +0900

    add test only

---
+++ _export.sh
@@ -0,0 +1,1 @@
+export CLASSPATH=.:/home/charsyam/pmd-src/lib/pmd-5.0.5.jar:/home/charsyam/pmd-src/lib/commons-io-2.2.jar:/home/charsyam/pmd-src/lib/jaxen-1.1.1.jar:/home/charsyam/pmd-src/lib/asm-3.2.jar
